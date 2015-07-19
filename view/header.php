750d0fc95aa816417912a17e48a683644d3fd4df
wangfulin <1622913826@qq.com>
2015-07-19 19:49:03 +0800

    add two files

---
+++ view/header.php
@@ -0,0 +1,7 @@
+  <!-- header start -->
+    <div class="header-wrapper">
+        <?php include "headerbar.php" ?>
+        <?php include "headersearch.php" ?>
+        <?php include "headernav.php" ?>
+    </div>
+   <!-- header end -->
