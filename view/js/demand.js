ae634811ff58c282f9da1e9efb0f9c789dfc0ddc
unknown <wangfulin@wangfulin-D3.corp.qihoo.net>
2015-08-18 09:49:56 +0800

    change demand.js

--- view/js/demand.js
+++ view/js/demand.js
@@ -5,13 +5,13 @@ $(".demand-list>ul>li>a").click(function(){
     $(this).nextAll(".demand-content-wrapper").css("display","block");
 });
 $(".show-detail").click(function(){
-    $(".demand-detail").css("display","block");
+    $(this).parent().parent().nextAll(".demand-detail").css("display","block");
     $(this).next(".hide-detail").css("display","block");
     $(this).css("display","none");
 });
 
 $(".hide-detail").click(function(){
-    $(".demand-detail").css("display","none");
+    $(this).parent().parent().nextAll(".demand-detail").css("display","none");
     $(this).prev(".show-detail").css("display","block");
     $(this).css("display","none");
 });
