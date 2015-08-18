1c05655f816b1a2df76cbb855e050834e259fb7f
unknown <wangfulin@wangfulin-D3.corp.qihoo.net>
2015-08-18 16:31:56 +0800

    首页修改了首部和尾部

---
+++ view/newIndex.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link type="text/css" href="css/reset.css" rel="stylesheet" />
+        <link type="text/css" href="css/font-awesome.min.css" rel="stylesheet" />
+        <link type="text/css" href="css/index.css" rel="stylesheet" />
+        <link rel="stylesheet" type="text/css" href="css/demand_new.css" />
+        <script src="js/jquery-2.1.3.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+    <?php
+        include "newHeader.php";
+    ?>
+
+    <!-- content-start -->
+    <div class="content-wrapper">
+        <div class="slider">
+            <a href="#"></a>
+        </div>
+        <div class="content">
+            <!-- 热门标签开始 -->
+            <div class="hot-cate">
+               <ul>
+               	  <li class="cap-icon label-icon"></li>
+                  <li class="cate">
+                  	<div class="caption">
+                    	<a href="#" target="_blank" rel="nofollow" class="more">更多</a>
+                    	<span class="cap-icon court-icon">场地类型</span>
+                    </div>
+                    <p>
+                    	<a href="#" rel="nofollow" target="_blank">五星级</a>
+                    	<a href="#" rel="nofollow" target="_blank">四星级</a>
+                    	<a href="#" rel="nofollow" target="_blank">三星级</a>
+                    	<a href="#" rel="nofollow" target="_blank">度假村</a>
+                    	<a href="#" rel="nofollow" target="_blank">农家乐</a>
+                    	<a href="#" rel="nofollow" target="_blank">中餐厅</a>
+                    	<a href="#" rel="nofollow" target="_blank">咖啡厅</a>
+                    	<a href="#" rel="nofollow" target="_blank">商圈 </a>
+                    </p>
+                  </li>
+                  <li class="cate">
+                  	<div class="caption">
+                    	<a href="#" rel="nofollow" target="_blank" class="more">更多</a>
+                    	<span class="cap-icon special-icon">特色标签</span>
+                    </div>
+                    <p>
+            			<a href="#" rel="nofollow" target="_blank">无柱会场</a>
+            			<a href="#" rel="nofollow" target="_blank">特色餐饮</a>
+            			<a href="#" rel="nofollow" target="_blank">自然采光</a>
+            			<a href="#" rel="nofollow" target="_blank">风景独特</a>
+            			<a href="#" rel="nofollow" target="_blank">大型会场</a>
+            			<a href="#" rel="nofollow" target="_blank">豪华酒店</a>
+                    </p>
+                  </li>
+                  <li class="cate">
+                  	<div class="caption">
+                    	<a href="#" target="_blank" class="more">更多</a>
+                    	<span class="cap-icon reco-icon">今日推荐</span>
+                    </div>
+                    <p id="today"><a href="http://www.taohuichang.com/supplier-6393.html" target="_blank">蓉城故事——成都书院（原成都会馆）</a><a href="http://www.taohuichang.com/supplier-474.html" target="_blank">东南亚风情-重庆东衡格兰维酒店</a></p>
+                    <p></p>
+                  </li>
+               </ul>
+            </div>
+            <!-- 热门标签结束 -->
+
+            <!-- 热门酒店推荐开始 -->
+            <div class="hot-hotel">
+                <div class="order-form-wrapper">
+                     <div class="order-form">
+                        <div class="order-form-txt">
+                            <h5>免费匹配3～5个优质地</h5>
+                            <p>
+                                <span>省去中间环节</span>
+                                <br />
+                                <span>商家快速报价</span>
+                            </p>
+                        </div>
+                        <div class="o-form">
+                            <form action="" method="">
+                                <div class="order-city">
+                                    <input class="txt-icon" type="text" placeholder="活动城市" />
+                                </div>
+                                <div class="order-name">
+                                    <input class="txt-icon" type="text" placeholder="您的称呼" />
+                                </div>
+                                <div class="order-contact">
+                                    <input class="txt-icon" type="text" placeholder="您的联系电话" />
+                                </div>
+                                <div class="order-btn">
+                                    <input type="submit" value="立即申请" />
+                                </div>
+                            </form>
+                        </div>
+                        <div class="notice-message">
+                            <p>
+                                <span class="curr-order-name">南京大学</span><span class="curr-order-phone">15903420323</span>
+                                <br />
+                                <br />
+                                <span>已获得免费场地方案</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="resource/hotel.jpg" title="" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="resource/hotel.jpg" title="" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="resource/hotel.jpg" title="" alt="" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- 热门酒店推荐结束 -->
+
+            <!-- 会议 培训 论坛开始 -->
+            <div class="meeting-wrapper">
+            <div class="meeting">
+                <div class="meeting-title">
+                    <h2>会议 培训 论坛</h2>
+                </div>
+                <div class="meeting-content">
+                    <div class="meeting-big-pic">
+                        <a href="#">
+                            <img src="resource/hotel.jpg" alt="" title="" />
+                        </a>
+                    </div>
+                    <div class="meeting-pic-area">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                             <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <!-- 会议 培训 论坛结束 -->
+            <br class="clearfix" />
+            <!-- 宴会，婚宴开始 -->
+            <div class="meeting-wrapper">
+            <div class="meeting">
+                <div class="meeting-title">
+                    <h2>宴会 婚宴</h2>
+                </div>
+                <div class="meeting-content">
+                    <div class="meeting-big-pic">
+                        <a href="#">
+                            <img src="resource/hotel.jpg" alt="" title="" />
+                        </a>
+                    </div>
+                    <div class="meeting-pic-area">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                             <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <!-- 宴会，婚宴结束 -->
+            <br class="clearfix" />
+            <!-- 聚会，沙龙，拓展开始 -->
+            <div class="meeting-wrapper">
+            <div class="meeting">
+                <div class="meeting-title">
+                    <h2>聚会 沙龙 拓展</h2>
+                </div>
+                <div class="meeting-content">
+                    <div class="meeting-big-pic">
+                        <a href="#">
+                            <img src="resource/hotel.jpg" alt="" title="" />
+                        </a>
+                    </div>
+                    <div class="meeting-pic-area">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                             <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="resource/place.jpg" alt="" title="" />
+                                    <em>金陵大酒店</em>
+                                    <br />
+                                    <span class="gray">1000平米 | 可容纳2000</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <!-- 聚会，沙龙，拓展结束 -->
+            <br class="clearfix" />
+        </div>
+    </div>
+    <!-- content end -->
+
+    <?php
+        include "newFooter.php";
+    ?>
+    </body>
+</html>
